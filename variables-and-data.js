c31d945a124d9d5afae53e0f9c894000480f1304
Trystan Gray <trystangray7@gmail.com>
2019-03-26 18:02:52 -0600

    getting familiar with variables, keys, strings, and types.

---
+++ variables-and-data.js
@@ -0,0 +1,14 @@
+let wordData;
+
+wordData = "Word List";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax;
+totalWithTax = tax + subTotal;
+
+
+
+
---
+++ variables-and-data3.js
@@ -0,0 +1,6 @@
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
